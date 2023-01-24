7eb80a934ec6bd1058e23f2d139283b493add3c2
Eric L. Hepperle <codewizard13@gmail.com>
2023-01-24 09:31:15 -0600

    REORG:  ADD gdrive/ gmail/ subdirs, each with index.js from quickstarts

--- googleapis-quickstart/gdrive/index.js
+++ googleapis-quickstart/gdrive/index.js
@@ -21,8 +21,8 @@ const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), '../private/gdrive.credentials.json');
+const TOKEN_PATH = path.join(`${__dirname}`, './token.json');
+const CREDENTIALS_PATH = path.join(`${__dirname}`, '../../private/gdrive.credentials.json');
 console.log({CREDENTIALS_PATH})
 
 /**
